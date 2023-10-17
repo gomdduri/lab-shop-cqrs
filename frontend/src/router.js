a2ac56ec2902382b386f036e66928312912df5dd
gomdduri <141398925+gomdduri@users.noreply.github.com>
2023-10-17 15:16:30 +0900

    commit

---
+++ frontend/src/router.js
@@ -0,0 +1,36 @@
+
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router);
+
+
+
+
+
+import Manager from "./components/listers/Cards"
+import Detail from "./components/listers/Detail"
+
+
+export default new Router({
+    // mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+
+
+
+            {
+                path: '//',
+                name: 'Manager',
+                component: Manager
+            },
+            {
+                path: '///:id',
+                name: 'Detail',
+                component: Detail
+            },
+
+
+
+    ]
+})
